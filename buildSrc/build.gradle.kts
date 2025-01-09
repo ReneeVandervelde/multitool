bef9d4ee42c741d3112a04bed6a65d95555ff6ad
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-01-08 19:08:45 -0600

    Configure kotlin plugin

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    `kotlin-dsl`
+}
+repositories {
+    gradlePluginPortal()
+    mavenCentral()
+    google()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(libs.kotlin.gradle)
+}
---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version("0.9.0")
+}
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
