11282cc016cd9a00c1ad417c724bcffdb36dd6dd
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-03-01 10:09:55 -0600

    Filter flatpak updates to command exists

--- system/src/main/kotlin/com/reneevandervelde/system/info/SystemInfo.kt
+++ system/src/main/kotlin/com/reneevandervelde/system/info/SystemInfo.kt
@@ -1,6 +1,19 @@
 package com.reneevandervelde.system.info
 
+import java.io.File
+
 data class SystemInfo(
     val operatingSystem: OperatingSystem,
-)
+    val executables: List<File>,
+) {
+    fun hasCommand(command: String): Boolean
+    {
+        return executables.any { it.name == command }
+    }
+
+    fun missingCommand(command: String): Boolean
+    {
+        return !hasCommand(command)
+    }
+}
 
