e2c37d1513e5fe57b811fd85607918737e661f2e
Renee Vandervelde <Renee@ReneeVandervelde.com>
2025-03-22 10:17:39 -0500

    Add list status to configuration command

--- system/src/main/kotlin/com/reneevandervelde/system/commands/configure/ConfigurationCommand.kt
+++ system/src/main/kotlin/com/reneevandervelde/system/commands/configure/ConfigurationCommand.kt
@@ -1,6 +1,7 @@
 package com.reneevandervelde.system.commands.configure
 
 import com.github.ajalt.clikt.parameters.arguments.argument
+import com.github.ajalt.clikt.parameters.arguments.optional
 import com.github.ajalt.clikt.parameters.types.enum
 import com.reneevandervelde.system.commands.SystemCommand
 import com.reneevandervelde.system.commands.configure.ConfigurationStatus.Configured
@@ -8,7 +9,9 @@ import com.reneevandervelde.system.commands.configure.ConfigurationStatus.Error
 import com.reneevandervelde.system.commands.configure.ConfigurationStatus.NotConfigured
 import com.reneevandervelde.system.exceptions.DocumentedResult
 import com.reneevandervelde.system.processes.ExitCode
+import com.reneevandervelde.system.render.println
 import ink.ui.structures.Sentiment
+import ink.ui.structures.TextStyle
 import ink.ui.structures.elements.StatusIndicatorElement
 
 object ConfigurationCommand: SystemCommand()
@@ -17,38 +20,65 @@ object ConfigurationCommand: SystemCommand()
         exitCode = ExitCode(12),
         meaning = "The specified configuration was not found",
     )
-    override val errors: List<DocumentedResult> = listOf(invalidConfigError)
+    private val missingConfigError = DocumentedResult(
+        exitCode = ExitCode(13),
+        meaning = "Configuration argument is required for this command",
+    )
+    override val errors: List<DocumentedResult> = listOf(
+        invalidConfigError,
+        missingConfigError,
+    )
     private val action by argument().enum<Verb>()
-    private val configuration by argument()
+    private val configuration by argument().optional()
 
     override suspend fun runCommand()
     {
-        val configurationClass = module.configurationModule
-            .configurations
-            .find { it.id == configuration }
-
-        if (configurationClass == null) {
-            invalidConfigError.throwError()
-        }
+        val configurationClass = if (configuration != null) {
+            module.configurationModule
+                .configurations
+                .find { it.id == configuration }
+                .also {
+                    if (it == null) {
+                        invalidConfigError.throwError()
+                    }
+                }
+        } else null
 
         when (action) {
             Verb.Status -> {
-                val status = configurationClass.getStatus()
-                output.print(StatusIndicatorElement(
-                    text = when (status) {
-                        is Configured -> "Configured"
-                        is NotConfigured -> "Not Configured"
-                        is Error -> "Error"
-                    },
-                    sentiment = when (status) {
-                        is Configured -> Sentiment.Positive
-                        is NotConfigured -> Sentiment.Nominal
-                        is Error -> Sentiment.Negative
+                if (configurationClass == null) {
+                    output.println("System Configurations", TextStyle.H1)
+                    module.configurationModule.configurations.forEach {
+                        output.println(it.id, TextStyle.H2)
+                        printStatus(it.getStatus())
                     }
-                ))
+                } else {
+                    printStatus(configurationClass.getStatus())
+                }
             }
-            Verb.Configure -> configurationClass.configure()
+            Verb.Configure -> {
+                if (configurationClass == null) {
+                    missingConfigError.throwError()
+                }
+                configurationClass.configure()
+            }
         }
+    }
+
+    private suspend fun printStatus(status: ConfigurationStatus)
+    {
+        output.print(StatusIndicatorElement(
+            text = when (status) {
+                is Configured -> "Configured"
+                is NotConfigured -> "Not Configured"
+                is Error -> "Error"
+            },
+            sentiment = when (status) {
+                is Configured -> Sentiment.Positive
+                is NotConfigured -> Sentiment.Nominal
+                is Error -> Sentiment.Negative
+            }
+        ))
     }
 
     enum class Verb
